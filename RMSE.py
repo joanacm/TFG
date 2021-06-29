85b73fd4a9c9d5b388eca1249c2cbcc0826a60b6
joanacm <72805479+joanacm@users.noreply.github.com>
2021-06-29 11:04:36 +0200

    Update RMSE.py

--- RMSE.py
+++ RMSE.py
@@ -37,41 +37,14 @@ def RMSE(mejora, inicial):
     t = t/dim[2]
     return t
 
-
-
-start2 = time.time()
-
 img_c_o = img.imread("imatges\image1\image1_interpolated.png")
 img_mej = img.imread("imatges\image1\img1_mejorada.png")
 
 img_c = np.copy(img_c_o)
 img_mejorada = np.copy(img_mej)
-# mejora = img_mejorada
-# inicial=img_c
-
-# dim = np.array(inicial.shape)
-# num_pixeles = dim[0]*dim[1]
-    
-# suma = 0
-# t = 0
-    
-# for c in (0, dim[2]):
-#     for i in (0, dim[1]):
-#         print("i=", i)
-#         for j in (0, dim[0]):
-#             print(dim[0])
-#             print("j=", j)
-#             print(mejora[j,i,c])
-#             print(inicial[j,i,c])
-#             suma = suma + (mejora[j,i,c]-inicial[j,i,c])**2
-#             print(suma)
-#     suma = m.sqrt(suma/num_pixeles)
-#     t = t + suma 
-    
-# t = t/dim[2]
 
 
-
+start2 = time.time()
 
 print(RMSE(img_mejorada, img_c))
 
