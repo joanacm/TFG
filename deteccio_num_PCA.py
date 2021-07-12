71cdd45fee3e3e7d384ac2fc5fd1bbaee2f95326
joanacm <72805479+joanacm@users.noreply.github.com>
2021-07-12 09:03:54 +0200

    Add files via upload

--- deteccio_num_PCA.py
+++ deteccio_num_PCA.py
@@ -23,46 +23,53 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 
+def distancia(test, train):
+    errores = np.zeros(len(train))
+    for i in range(0,len(train)):
+        for j in range(0, len(test)):
+            errores[i] = errores[i] + (train[i][j] - test[j])*(train[i][j] - test[j])
+        errores[i] = m.sqrt(errores[i])
+    return errores
+
+
 start = time.time()
 
 test = pd.read_csv('data/mnist/test.csv')
 train = pd.read_csv('data/mnist/train.csv')
 
+#guardamos que número representa cada imagen (fila)
 num_train = train['label']
+#eliminamos la variable label 
 del(train['label'])
 
-test.head()
-
+#guardamos que número representa cada imagen (fila)
+num_test = test['label']
+#eliminamos la variable label 
+del(test['label'])
 
 #realitzamos una PCA en train
 pca = PCA()
 train_proy = pca.fit_transform(train)
 veps = pca.components_
 
-#cargamos una imagen de un dígito desconocido
-ejemplo_test = np.array(test.iloc[2].values)
+for i in range(0, len(num_test)):
+    #cargamos una imagen de un dígito desconocido
+    ejemplo_test = np.array(test.iloc[i].values)
 
-#projectam l'exemple a les coordenades anteriors (veps)
-test_proy = np.dot(veps, ejemplo_test)
-
-#calculamos las distancias
-errores = np.zeros(len(num_train))
-for i in range(0,len(num_train)):
-    for j in range(0, 28*28):
-        errores[i] = errores[i] + abs(train_proy[i][j] - test_proy[j] )
-
-
-#nos quedamos con aquella imagen que se parezca mas a la queremos testear
-index = np.argmin(errores)
-
-#Mostramos que numero representa la imagen con distancia minima 
-#y que suponemos que es como la que queremos testear
-print("El numero que se muestra en esta imagen es " + str(num_train[index]) )
-
-
-nueve = test.iloc[2].values.reshape([28,28])
-plt.imshow(cero, cmap='gray_r')
-plt.savefig("imatges/deteccion/nosetest.png")
+    #projectam l'exemple a les coordenades anteriors (veps)
+    test_proy = np.dot(veps, ejemplo_test)
+    
+    errores = distancia(test_proy, train_proy)
+    
+    index_train = num_train[np.argmin(errores)]
+    index_text = num_test[i]
+    
+    #comprobamos si se ha acertado
+    if index_text != index_train:
+        test = test.iloc[i].values.reshape([28,28])
+        plt.imshow(test, cmap='gray_r')
+        plt.title(u'Resultado: %s' % str(index_train))
+        plt.savefig("imatges\deteccion\image_erronea_%d.png"%(i+1))
 
 
 end = time.time()
